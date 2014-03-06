d23a9947319be3bcae67bafba8aeceff54f301e7
Gonzalo J. García Martín <=>
2014-03-06 16:50:02 +0000

    tokens.js cubierto completamente

--- tests/tests.js
+++ tests/tests.js
@@ -4,17 +4,16 @@ suite('Javascript Lexer', function() {
     test('Asignacion, Funcion tokens', function() {
 		 var input = "var a = b;";
 		 var resultado = JSON.stringify(input.tokens());
-		  var esperado = "[{\"type\":\"name\",\"value\":\"var\",\"from\":0,\"to\":3},{\"type\":\"name\",\"value\":\"a\",\"from\":4,\"to\":5},{\"type\":\"operator\",\"value\":\"=\",\"from\":6,\"to\":7},{\"type\":\"name\",\"value\":\"b\",\"from\":8,\"to\":9},{\"type\":\"operator\",\"value\":\";\",\"from\":9,\"to\":10}]";
-		  
-        assert.deepEqual();
+		 var esperado = "[{\"type\":\"name\",\"value\":\"var\",\"from\":0,\"to\":3},{\"type\":\"name\",\"value\":\"a\",\"from\":4,\"to\":5},{\"type\":\"operator\",\"value\":\"=\",\"from\":6,\"to\":7},{\"type\":\"name\",\"value\":\"b\",\"from\":8,\"to\":9},{\"type\":\"operator\",\"value\":\";\",\"from\":9,\"to\":10}]";
+       assert.deepEqual(esperado, resultado);
     });
-//      test(';Comentario', function() {
-//          var s = ";Comentario";
-// 		   var t = lexer(s);
-// 			var c = t[0].match;
-//          assert.equal(c[0], ';Comentario');
-// 			assert.equal(c[1], 'Comentario');
-//      });
+      test('Esto es un comentario, funcion bexec', function() {
+        var input = "/*Esto es un comentario Comentario*/";
+ 		  var regexp = /\/\*(.|\n)*?\*\//;
+		  regexp.lastIndex = 0;
+		  var resultado = regexp.bexec(input);
+		  assert.equal(input, resultado[0]);
+      });
 // 	  test('Nombre = Valor', function() {
 // 		  var s = "Nombre = Gonzalo";
 // 		  var t = lexer(s);
